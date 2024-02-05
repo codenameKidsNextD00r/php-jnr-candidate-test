e08da22aa3e633fa81b6b11885c4aec45d99f519
codenameKidsNextD00r <mlumbibongani@gmail.com>
2024-02-05 07:42:43 +0200

    added products array and declared it as an object of the Product class. Completed successful function call

--- task2.php
+++ task2.php
@@ -8,4 +8,32 @@ Class Product
     {
         $this->products = $products;
     }
+
+    public function get_products($productID)
+    {
+        foreach($this->products as $product){
+            if($productID == $product['id']){
+                return $product;
+            }
+        }
+    }
+}
+
+$products = [
+    ['id' => 101, 'name' => 'Product 1', 'price' => 99.99],
+    ['id' => 102, 'name' => 'Product 2', 'price' => 75.00],
+    ['id' => 103, 'name' => 'Product 3', 'price' => 120.00],
+];
+
+$productObject = new Product($products);
+$productId = $products['id'];
+
+//enter id here
+$product = $productObject->get_products(11); 
+
+if (empty($product)) {
+    echo "That product does not exist\n";
+} else {
+    echo 'Product Name: ' . $product['name'] . "\n";
+    echo 'Product Price: ' . $product['price'] . "\n";
 }
